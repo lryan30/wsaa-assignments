bd3f04fd7a54c7988907e31b445a1f1b97d217d1
Louise Ryan <louisearyan30@gmail.com>
2025-03-24 20:19:34 +0000

    updated assignment 03

--- assignments/assignment03-cso.py
+++ assignments/assignment03-cso.py
@@ -2,25 +2,33 @@
 
 # Write a program that retrieves the dataset for the "exchequer account (historical series)" from the CSO, 
 # and store into a file called "cso.json". 
+# Author: LR
 
-import requests
+import requests  
 import json
 
-url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/XLSX/2007/en"
+# URL to retrieve the Exchequer Account Historical Series dataset in JSON-stat format
+url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en"
 
+# Send GET request to retrieve the data from the URL. 
+response = requests.get(url)
+    
+data = response.json()   
+ 
+with open('cso.json', "w") as json_file: # open the file in write mode which creates the file.
+        json.dump(data, json_file)  # json dump is used to write the data to the file in json format
 
+print(f"Data has been saved in cso.json")
 
-
-
-
-
-
+    
 
 
 
 # References: 
+# https://data.cso.ie/
+# https://data.gov.ie/dataset/fiq02-exchequer-account-historical-series
+#  
 
-#   https://ws.cso.ie/
 
 
 
