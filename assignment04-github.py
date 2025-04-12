755560864913eacbca3780fe356220285c48b40c
Louise Ryan <louisearyan30@gmail.com>
2025-04-12 19:52:27 +0100

    added reference

--- assignment04-github.py
+++ assignment04-github.py
@@ -1,6 +1,8 @@
-
-
-
+# Assignment 04:
+# This script uses the GitHub API to access a file in a github repository using an API key.
+# It downloads the file from the repository, replaces the name "Andrew" with "Louise",
+# and then uploads the edited file back to the repository. 
+# Author: Louise Ryan
 
 
 from github import Github
@@ -16,20 +18,19 @@ repo = g.get_repo("lryan30/wsaa-assignments")
 # file path:
 file_path = "file.txt"  
 
-# Get the file content
-file_content = repo.get_contents(file_path)
+# Get the file content 
+file_content = repo.get_contents(file_path) 
 
-#get url of the file
-url = file_content.download_url
+#get download url of the raw file 
+url = file_content.download_url 
 print(url)
 
-# Get the file content using requests
+# Get the file content from the url using requests module
 response = requests.get(url)
-
 file_data = response.text
 print(file_data)
 
-# replace Andrew with Louise
+# replace Andrew with Louise 
 file_data = file_data.replace("Andrew", "Louise")
 print(file_data)
 
@@ -37,6 +38,13 @@ print(file_data)
 repo.update_file(file_path, "Updated file", file_data, file_content.sha)
 print("File updated successfully!")
 
+
+
+# References: 
+# https://pygithub.readthedocs.io/en/stable/examples/Repository.html#update-a-file-in-the-repository
+# https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#creating-a-fine-grained-personal-access-token
+# https://github.com/PyGithub/PyGithub/issues/1343
+# https://www.w3schools.com/python/ref_string_replace.asp
 
 
 
